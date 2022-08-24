188854fdf6526dc467172c68ea5cdb9b242d242d
bishetheanswer <migueljativa4@gmail.com>
2022-08-24 21:31:05 +0200

    Add logs to itbooks and nytimes

--- lambda/get-books-itbooks/app.py
+++ lambda/get-books-itbooks/app.py
@@ -1,88 +1,116 @@
 import json
 import os
-from faker import Faker
 import requests
 from datetime import datetime
+import unicodedata
 import boto3
 import logging
-# import re
-import unicodedata
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 S3_BUCKET = os.environ.get("S3_BUCKET")
 DYNAMO_TABLE = os.environ.get("DYNAMO_TABLE")
 
 
 def handler(event, context):
-    s3 = boto3.client("s3")
+    """Get books from the itbooks API, store raw data and new books to S3"""
+    s3_client = boto3.client("s3")
+    logging.info("Requesting books...")
     r = requests.get("https://api.itbook.store/1.0/new").json()
-    store_raw_data(r, s3)
+    logging.info("Storing raw data in S3...")
+    store_raw_data(r, s3_client)
     books = r["books"]
-    new_books = [book for book in books if not exists_in_mongo(book)]
+    logging.info("Checking number of new books...")
+    new_books = [book for book in books if not exists_in_dynamo(book)]
+    if len(new_books) == 0:
+        logging.info("There are no new books!")
+        return []
+    logging.info(f"There are {len(new_books)} new books!")
     all_new_keys = []
     for book in new_books:
-        book_details = get_book_details(book)  # los libros pueden tener varios autores
-        uploaded_keys = upload_to_s3(book_details)
+        logging.info("Getting book details...")
+        book_details = get_book_details(book)
+        logging.info(f"Uploading {book['title']}...")
+        uploaded_keys = upload_to_s3(book_details, s3_client)
         all_new_keys.extend(uploaded_keys)
     return all_new_keys
 
-def exists_in_mongo(book):
-    dynamo_client = boto3.client('dynamodb', region_name='us-east-1')
 
-    isbn13 = book['isbn13']
-    condition = 'isbn13 = :isbn13'
-    attributes = {':isbn13': {'S': isbn13}}
-    response = dynamo_client.query(TableName=DYNAMO_TABLE, KeyConditionExpression=condition, ExpressionAttributeValues=attributes)
-    print(isbn13, response)
-    matching_books = response.get('Items', [])
+def exists_in_dynamo(book):
+    """Check whether a book exists in DynamoDB or not based on its ISBN13"""
+    dynamo_client = boto3.client("dynamodb", region_name="us-east-1")
+
+    isbn13 = book["isbn13"]
+    condition = "isbn13 = :isbn13"
+    attributes = {":isbn13": {"S": isbn13}}
+    try:
+        response = dynamo_client.query(
+            TableName=DYNAMO_TABLE,
+            KeyConditionExpression=condition,
+            ExpressionAttributeValues=attributes,
+        )
+    except Exception as e:
+        logging.exception("There was a problem while checking in DynamoDB!")
+        raise e
+    matching_books = response.get("Items", [])
     if len(matching_books) != 0:
-        logging.info(f"Book {book['title']} already existst!")
-        print('Books already exists!')
         return True
-    
-    # # TODO: usar un query en vez de un scan, para eso hay que hace que isbn13 sea primary key o como se llame
-    # response = dynamo_client.scan(TableName=DYNAMO_TABLE, IndexName='isbn13', FilterExpression=condition, ExpressionAttributeValues=attributes)
-    # matching_books = response.get('Items', [])
-    # if len(matching_books) != 0:
-    #     logging.info(f"Book {book['title']} already existst!")
-    #     return True
     return False
 
 
 def get_book_details(book):
+    """Get book details using its ISBN13 and the itbooks API"""
     return requests.get(f"https://api.itbook.store/1.0/books/{book['isbn13']}").json()
 
 
-def upload_to_s3(book):
+def upload_to_s3(book, s3):
+    """Upload book details as json in S3.
+
+    If a book has more than one author, duplicate the book as many times as authors
+    """
     uploaded_keys = []
-    s3 = boto3.client("s3")
-    authors = book["authors"].split(", ")  # puede haber mas de un autor
+    # s3 = boto3.client("s3")
+    authors = book["authors"].split(", ")  # authors are separated by a ","
     for author in authors:
         author = normalize_name(author)
         key = f"clean/{author}/{book['isbn13']}.json"
-        s3.put_object(Body=json.dumps(book), Bucket=S3_BUCKET, Key=key)
+        try:
+            s3.put_object(Body=json.dumps(book), Bucket=S3_BUCKET, Key=key)
+        except Exception as e:
+            logging.exception("There was a problem uploading the book to S3!")
+            raise e
         uploaded_keys.append(key)
     return uploaded_keys
 
-def store_raw_data(response, s3):
-    """Store the raw response from an API in S3
 
-    Args:
-        response (dict): API response
-        s3 (client): S3 boto3 client
-    """
+def store_raw_data(response, s3):
+    """Store the raw response from an API in S3"""
     today = datetime.today().strftime("%Y-%m-%d")
-    s3.put_object(
-        Body=json.dumps(response), Bucket=S3_BUCKET, Key=f"raw/itbooks-api/{today}.json"
-    )
+    try:
+        s3.put_object(
+            Body=json.dumps(response),
+            Bucket=S3_BUCKET,
+            Key=f"raw/itbooks-api/{today}.json",
+        )
+    except Exception as e:
+        logging.exception("There was a problem uploading the raw data to S3!")
+        raise e
 
 
 def strip_accents(s):
-    # https://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-normalize-in-a-python-unicode-string
-    return ''.join(c for c in unicodedata.normalize('NFD', s)
-                  if unicodedata.category(c) != 'Mn')
+    """Get rid of accents
+
+    https://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-normalize-in-a-python-unicode-string
+    """
+    return "".join(
+        c for c in unicodedata.normalize("NFD", s) if unicodedata.category(c) != "Mn"
+    )
 
 
 def normalize_name(s):
-    s = s.replace(".", "").replace(" ","_").lower() # el formato suele ser nombre apellido o nombre inicial. apellido
+    """Remove dots, underscores and accents"""
+    s = s.replace(".", "").replace(" ", "_").lower()
     s = strip_accents(s)
     return s
--- lambda/get-books-nytimes/app.py
+++ lambda/get-books-nytimes/app.py
@@ -1,11 +1,13 @@
 import json
 import os
-from faker import Faker
 import requests
 from datetime import datetime
 import boto3
 import unicodedata
 import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 S3_BUCKET = os.environ.get("S3_BUCKET")
@@ -14,92 +16,128 @@ SSM_PARAM = os.environ.get("SSM_PARAM")
 
 
 def handler(event, context):
-    print("NYTIMES")
-    body = {
-        "message": f"Getting books from The New York Times API {S3_BUCKET} and {DYNAMO_TABLE}",
-        "input": event,
-    }
-    fake = Faker()
+    """Get books from the nytimes API, store raw data and new books to S3"""
     ssm_client = boto3.client("ssm", region_name="us-east-1")
-    # get all the list's names
-    # https://api.nytimes.com/svc/books/v3/lists/names.json
+    s3_client = boto3.client("s3")
 
-    # for each list name get the latest list
-    response = ssm_client.get_parameter(
-        Name=SSM_PARAM, WithDecryption=True
-    )  # TODO env var name
-    api_key = response["Parameter"]["Value"]
+    ssm_response = ssm_client.get_parameter(Name=SSM_PARAM, WithDecryption=True)
+    api_key = ssm_response["Parameter"]["Value"]
+    logging.info("Getting book categories available...")
+    book_categories = get_book_categories(api_key)
+    uploaded_keys = []
+    for category in book_categories:
+        try:
+            process_books_from_category(category, api_key, uploaded_keys, s3_client)
+        except:
+            logging.warning(
+                f"There was a problem getting {category} books skipping to the next one..."
+            )
+    return uploaded_keys
 
-    list_name = "mass-market-paperback"
+
+def process_books_from_category(category, api_key, uploaded_keys, s3):
+    """Process books from the given category"""
+    logging.info(f"Getting {category} books...")
     r = requests.get(
-        f"https://api.nytimes.com/svc/books/v3/lists.json?api-key={api_key}&list={list_name}"
+        f"https://api.nytimes.com/svc/books/v3/lists.json?api-key={api_key}&list={category}"
     ).json()
-
-    # store the raw file in s3
-    today = datetime.today().strftime("%Y-%m-%d")
-    s3 = boto3.client("s3")
-    s3.put_object(
-        Body=json.dumps(r),
-        Bucket=S3_BUCKET,
-        Key=f"raw/nytimes-api/{today}/{list_name}.json",
-    )
-
-    all_new_keys = []
+    logging.info(f"Storing raw {category} books data to S3")
+    store_raw_data(r, s3, category)
+    logging.info(f"Extracting {category} books details...")
     books = extract_books(r)
-    new_books = [book for book in books if not exists_in_mongo(book)]
+    logging.info("Checking number of new books...")
+    new_books = [book for book in books if not exists_in_dynamo(book)]
+    if len(new_books) == 0:
+        logging.info(f"There are no {category} new books!")
+        return
+    logging.info(f"There are {len(new_books)} new {category} books!")
     for book in new_books:
-        key = upload_to_s3(book)
-        print('Uploaded', book)
-        all_new_keys.append(key)
+        logging.info(f"Uploading {book['title']}...")
+        key = upload_to_s3(book, s3)
+        uploaded_keys.append(key)
 
-    return all_new_keys
+
+def get_book_categories(api_key):
+    """Get the list of book categories available in the API"""
+    r = requests.get(
+        f"https://api.nytimes.com/svc/books/v3/lists/names.json?api-key={api_key}"
+    ).json()
+    results = r["results"]
+    return [l["list_name_encoded"] for l in results]
+
+
+def store_raw_data(response, s3, category):
+    """Store the raw response from an API in S3"""
+    today = datetime.today().strftime("%Y-%m-%d")
+    try:
+        s3.put_object(
+            Body=json.dumps(response),
+            Bucket=S3_BUCKET,
+            Key=f"raw/nytimes-api/{today}/{category}.json",
+        )
+    except Exception as e:
+        logging.exception("There was a problem uploading the raw data to S3!")
+        raise e
 
 
 def extract_books(response):
+    """Extract books from the API response"""
     books = []
     for result in response["results"]:
         books.append(result["book_details"][0])
     return books
 
 
-def exists_in_mongo(book):
-    dynamo_client = boto3.client('dynamodb', region_name='us-east-1')
+def exists_in_dynamo(book):
+    """Check whether a book exists in DynamoDB or not based on its ISBN13"""
+    dynamo_client = boto3.client("dynamodb", region_name="us-east-1")
 
-    isbn13 = book['primary_isbn13']
-    condition = 'isbn13 = :isbn13'
-    attributes = {':isbn13': {'S': isbn13}}
-    response = dynamo_client.query(TableName=DYNAMO_TABLE, KeyConditionExpression=condition, ExpressionAttributeValues=attributes)
-    matching_books = response.get('Items', [])
+    isbn13 = book["primary_isbn13"]
+    condition = "isbn13 = :isbn13"
+    attributes = {":isbn13": {"S": isbn13}}
+    try:
+        response = dynamo_client.query(
+            TableName=DYNAMO_TABLE,
+            KeyConditionExpression=condition,
+            ExpressionAttributeValues=attributes,
+        )
+    except Exception as e:
+        logging.exception("There was a problem while checking in DynamoDB!")
+        raise e
+    matching_books = response.get("Items", [])
     if len(matching_books) != 0:
-        logging.info(f"Book {book['title']} already existst!")
-        print('Books already exists!')
         return True
-    
-    # # TODO: usar un query en vez de un scan, para eso hay que hace que isbn13 sea primary key o como se llame
-    # response = dynamo_client.scan(TableName=DYNAMO_TABLE, IndexName='isbn13', FilterExpression=condition, ExpressionAttributeValues=attributes)
-    # matching_books = response.get('Items', [])
-    # if len(matching_books) != 0:
-    #     logging.info(f"Book {book['title']} already existst!")
-    #     return True
     return False
 
 
-def upload_to_s3(book):
-    s3 = boto3.client("s3")
+def upload_to_s3(book, s3):
+    """Upload book details as json in S3.
+
+    # TODO: If a book has more than one author, duplicate the book as many times as authors
+    """
     author = normalize_name(book["author"])
     isbn13 = book["primary_isbn13"]
     key = f"clean/{author}/{isbn13}.json"
-    s3.put_object(Body=json.dumps(book), Bucket=S3_BUCKET, Key=key)
+    try:
+        s3.put_object(Body=json.dumps(book), Bucket=S3_BUCKET, Key=key)
+    except Exception as e:
+        logging.exception("There was a problem uploading the book to S3!")
+        raise e
     return key
 
 
 def strip_accents(s):
-    # https://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-normalize-in-a-python-unicode-string
-    return ''.join(c for c in unicodedata.normalize('NFD', s)
-                  if unicodedata.category(c) != 'Mn')
+    """Get rid of accents
+
+    https://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-normalize-in-a-python-unicode-string
+    """
+    return "".join(
+        c for c in unicodedata.normalize("NFD", s) if unicodedata.category(c) != "Mn"
+    )
 
 
 def normalize_name(s):
-    s = s.replace(".", "").replace(" ","_").lower() # el formato suele ser nombre apellido o nombre inicial. apellido
+    """Remove dots, underscores and accents"""
+    s = s.replace(".", "").replace(" ", "_").lower()
     s = strip_accents(s)
     return s
