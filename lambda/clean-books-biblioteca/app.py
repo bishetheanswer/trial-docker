de297fc0875db1c715f57463c0b170e36be6f548
bishetheanswer <migueljativa4@gmail.com>
2022-09-10 21:49:50 +0200

    Add logs and try catch to clean books biblioteca

--- lambda/clean-books-biblioteca/app.py
+++ lambda/clean-books-biblioteca/app.py
@@ -17,6 +17,7 @@ DYNAMO_TABLE = os.environ.get("DYNAMO_TABLE")
 
 def handler(event, context):
     csv_file = event["csv_key"]
+    logging.info(f"CSV file: {csv_file}")
     csv_path = download_from_s3(key=csv_file, file_path="/tmp/books.csv")
     df = pd.read_csv(csv_path, sep=";")
     df = drop_rows_and_cols(df)
@@ -44,13 +45,14 @@ def handler(event, context):
         ]
     ]
     df = df.fillna("")
-    df = df.drop_duplicates(subset=['Autor Personas', 'Título'])
+    df = df.drop_duplicates(subset=["Autor Personas", "Título"])
     uploaded_keys = upload_to_s3(df)
     return uploaded_keys
 
 
 def drop_rows_and_cols(df):
     """Drop empty rows and columns. Also drop rows without an author"""
+    logging.info("Dropping empty rows and columns...")
     df = df.copy()
     df = df.dropna(how="all", axis=0)
     df = df.dropna(how="all", axis=1)
@@ -60,6 +62,8 @@ def drop_rows_and_cols(df):
 
 
 def clean_author_names(df):
+    """Apply an exhaustive cleaning process to the authors"""
+    logging.info("Cleaning author names...")
     df = df.copy()
     df[AUTHOR_COL] = (
         df[AUTHOR_COL]
@@ -85,6 +89,7 @@ def clean_author_names(df):
 
 
 def initial_clean(s):
+    """Lowercase and remove digits and other symbols"""
     s = re.sub("\d", "", s)  # get rid of digits
     s = re.sub("\(.*\)", "", s)  # get rid of parenthesis and what is inside
     s = re.sub(
@@ -95,10 +100,12 @@ def initial_clean(s):
 
 
 def remove_empty_list_position(l):
+    """Return the last element of a list"""
     return l[:-1]
 
 
 def swap_name_and_surname(l):
+    """Swap the name and surname positions in the list containing surname, name and title"""
     # the format is [surname, name, title]
     if len(l) == 1:
         return l
@@ -110,6 +117,7 @@ def swap_name_and_surname(l):
 
 
 def treat_whitespaces(l):
+    """Remove whitespaces in the beginning and ending and substitute the one in the middle with an underscore"""
     for i in range(len(l)):
         l[i] = re.sub("^\ +", "", l[i])
         l[i] = re.sub("\ +$", "", l[i])
@@ -120,34 +128,51 @@ def treat_whitespaces(l):
 
 
 def strip_accents(s):
-    # https://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-normalize-in-a-python-unicode-string
+    """Get rid of accents
+
+    https://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-normalize-in-a-python-unicode-string
+    """
     return "".join(
         c for c in unicodedata.normalize("NFD", s) if unicodedata.category(c) != "Mn"
     )
 
 
 def keep_chars_and_separator(s):
+    """Keep letters and underscores"""
     return re.sub("[^a-zA-Z_]", "", s)
 
 
 def clean_start_and_end(s):
+    """Remove underscores in the beginning and ending"""
     s = re.sub("^_", "", s)
     s = re.sub("_$", "", s)
     return s
 
 
 def upload_to_s3(df):
+    """Upload books data to S3"""
     s3 = boto3.client("s3")
     uploaded_keys = []
     for _, row in df.iterrows():
         book = row.to_dict()
         key = f"clean/{book[AUTHOR_COL]}/{book['idBNE']}.json"
-        s3.put_object(Body=json.dumps(book), Bucket=S3_BUCKET, Key=key)
-        uploaded_keys.append(key)
+        logging.info(f"Uploading {key}...")
+        try:
+            s3.put_object(Body=json.dumps(book), Bucket=S3_BUCKET, Key=key)
+            uploaded_keys.append(key)
+        except Exception as e:
+            logging.warning(f"There was problem uploading {key}")
+
     return uploaded_keys
 
 
 def download_from_s3(key, file_path):
+    """Download file from S3"""
     s3 = boto3.client("s3")
-    s3.download_file(S3_BUCKET, key, file_path)
+    logging.info(f"Downloading {key}...")
+    try:
+        s3.download_file(S3_BUCKET, key, file_path)
+    except Exception as e:
+        logging.exception(f"There was a problem downloading {key}")
+        raise e
     return file_path
